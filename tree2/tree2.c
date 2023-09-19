b496f37e70a50a75a7320bd9a382e9cb05ed8a17
Gyeonghwan <kkyungwhan@naver.com>
2023-09-19 10:35:14 +0900

    0919

--- tree2/tree2.c
+++ tree2/tree2.c
@@ -1,111 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
 
 typedef struct TreeNode {
-	int data;
-	struct TreeNode* left, * right;
-}TreeNode;
+    char data;
+    struct TreeNode* left, * right;
+} TreeNode;
 
-#define SIZE 100
-int top = -1;
-TreeNode* stack[SIZE];
+// ((((2+3)+(4*5))-(6/7))+9)
 
-void push(TreeNode* p)
+TreeNode n1 = { '2', NULL, NULL };
+TreeNode n2 = { '3', NULL, NULL };
+TreeNode n3 = { '+', &n1, &n2 };
+TreeNode n4 = { '4', NULL, NULL };
+TreeNode n5 = { '5', NULL, NULL };
+TreeNode n6 = { '*', &n4, &n5 };
+TreeNode n7 = { '+', &n3, &n6 };
+TreeNode n8 = { '6', NULL, NULL };
+TreeNode n9 = { '7', NULL, NULL };
+TreeNode n10 = { '/', &n8, &n9 };
+TreeNode n11 = { '-', &n7, &n10 };
+TreeNode n12 = { '9', NULL, NULL };
+TreeNode n13 = { '+', &n11, &n12 };
+
+TreeNode* exp = &n13;
+
+// 수식 계산 함수
+double evaluate(TreeNode* root)
 {
-	if (top < SIZE - 1)
-		stack[++top] = p;
+    if (root == NULL)   return 0;
+    if (root->left == NULL && root->right == NULL) {
+        return atof(&root->data); // 문자를 숫자로 변환
+    }
+    else {
+        double op1 = evaluate(root->left);
+        double op2 = evaluate(root->right);
+        printf("%.2lf %c %.2lf을 계산합니다.\n", op1, root->data, op2);
+        switch (root->data) {
+        case '+':   return op1 + op2;
+        case '-':   return op1 - op2;
+        case '*':   return op1 * op2;
+        case '/':   return op1 / op2;
+        }
+    }
+    return 0;
 }
 
-TreeNode* pop()
+//노드의 개수를 구하는 함수
+int get_node_count(TreeNode* node)
 {
-	TreeNode* p = NULL;
-	if (top >= 0)
-		p = stack[top--];
-	return p;
+    int count = 0;
+    if (node != NULL)
+        count = 1 + get_node_count(node->left) + //root는 하나이므로 1를 더하고 시작함
+        get_node_count(node->right); //순회하며 노드의 개수를 더함
+    return count;
 }
-
-int is_empty()
-{
-	return top == -1;
-}
-
-void inorder_iter(TreeNode* root)
-{
-	while (1) {
-		for (; root; root = root->left)
-			push(root);
-		root = pop();
-		if (!root) break;
-		printf("[%d] ", root->data);
-		root = root->right;
-	}
-}
-
-void preorder_iter(TreeNode* root)
-{
-	push(root);
-	while (!is_empty()) {
-		root = pop();
-		printf("[%d] ", root->data);
-
-		if (root->right)
-			push(root->right);
-		if (root->left)
-			push(root->left);
-	}
-}
-
-
-void postorder_iter(TreeNode* root)
-{
-	if (root == NULL)
-		return;
-
-	TreeNode* prev = NULL;
-	do {
-		while (root) {
-			push(root);
-			root = root->left;
-		}
-
-		while (root == NULL && top >= 0) {
-			root = stack[top];
-			if (root->right == NULL || root->right == prev) {
-				printf("[%d] ", root->data);
-				pop();
-				prev = root;
-				root = NULL;
-			}
-			else {
-				root = root->right;
-			}
-		}
-	} while (top >= 0);
-}
-
-TreeNode n1 = { 1, NULL, NULL };
-TreeNode n2 = { 4, &n1, NULL };
-TreeNode n3 = { 16, NULL, NULL };
-TreeNode n4 = { 25, NULL, NULL };
-TreeNode n5 = { 20, &n3, &n4 };
-TreeNode n6 = { 15, &n2, &n5 };
-TreeNode* root = &n6;
 
 int main(void)
 {
-	printf("중위 순회=");
-	inorder_iter(root);
-	printf("\n");
+    printf("수식의 값은 %.2lf입니다. \n\n", evaluate(exp));
 
-	printf("전위 순회=");
-	preorder_iter(root);
-	printf("\n");
+    int nodecount = get_node_count(exp);
+    printf("총 노드 개수는 %d입니다. \n", nodecount);
 
-	printf("후위 순회=");
-	postorder_iter(root);
-	printf("\n");
-
-	return 0;
+    return 0;
 }
-
